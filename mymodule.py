f8c0f1bfa9332503c3b6e518b142d3903f4efdcd
Andreas Stocker <AStocker@anexia-it.com>
2020-11-10 07:45:39 +0100

    Initial commit

---
+++ mymodule.py
@@ -0,0 +1,20 @@
+from mydata import *
+
+
+def myfunction(event, conext):
+    call_stack_1()
+
+def call_stack_1():
+    call_stack_2()
+
+def call_stack_2():
+    call_stack_3()
+
+def call_stack_3():
+    call_stack_4()
+
+def call_stack_4():
+    print(TEXT_1)
+    print(TEXT_2)
+    print(TEXT_3)
+
